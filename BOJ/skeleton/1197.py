894a25b1ac07d530886fe3e3f9fe2553f8383305
fightnyy <fightnyy@naver.com>
2021-05-12 03:11:49 +0000

    1197

--- BOJ/skeleton/1197.py
+++ BOJ/skeleton/1197.py
@@ -1,28 +1,83 @@
 import heapq
 
+def find_parent(node):
+    if node != parent[node]:
+        parent[node]=find_parent(parent[node])
+    return parent[node]
+
+
+
+
+def check_same_parent(node1, node2):
+    node1= find_parent(node1)
+    node2 = find_parent(node2)
+    if node1 > node2:
+        parent[node1] = node2
+    elif node2 > node1:
+        parent[node2] = node1
+
+
+
+
+
 if __name__ == "__main__":
     V, E = map(int, input().split())
-    visited = [False] * V
+    parent = [i for i in range(V)] 
     answer = 0
     q = []
     for i in range(E):
         start, end, cost = map(int, input().split())
         start -= 1
         end -= 1
-        heapq.heappush(q,[cost, start, end]) #걸리는 시간, 시작점, 도착점
+        heapq.heappush(q, [cost, start, end])  # 걸리는 시간, 시작점, 도착점
     while q:
-        # import pdb; pdb.set_trace()
         tmp = heapq.heappop(q)
-        for val in q : #val == 걸리는 시간, 시작점, 도착점
-            tmp_list = []
-            if tmp[2] == val[1] and not visited[val[1]]: #tmp의 도착점과 val의 시작점이 같고 val을 한번도 방문한적이 없으면
-                tmp_list.append(val)
-
-        for a in tmp_list:
-            answer += a[0]
-            heapq.heappush(q,[a[0],a[1],a[2]])
-            visited[a[1]] = True
-            visited[a[2]] = True
-    print(visited)
+        if find_parent(tmp[1]) != find_parent(tmp[2]):
+            check_same_parent(tmp[1],tmp[2])
+            answer += tmp[0]
     print(answer)
+    print(parent)
 
+
+
+
+
+# import heapq
+
+# def find_parent(node):
+#     if node == parent[node]:
+#         return node
+#     parent[node]=find_parent(parent[node])
+#     return parent[node]
+        
+
+
+# def check_same_parent(node1, node2):
+#     node1= find_parent(node1)
+#     node2 = find_parent(node2)
+#     if node1 > node2:
+#         parent[node1] = node2
+#     elif node2 > node1:
+#         parent[node2] = node1
+
+
+
+
+
+# if __name__ == "__main__":
+#     V, E = map(int, input().split())
+#     parent = [i for i in range(V)] 
+#     answer = 0
+#     q = []
+#     for i in range(E):
+#         start, end, cost = map(int, input().split())
+#         start -= 1
+#         end -= 1
+#         heapq.heappush(q, [cost, start, end])  # 걸리는 시간, 시작점, 도착점
+#     while q:
+#         tmp = heapq.heappop(q)
+#         if find_parent(tmp[1]) != find_parent(tmp[2]):
+#             check_same_parent(tmp[1],tmp[2])
+#             answer += tmp[0]
+#     print(answer)
+#     # print(parent)
