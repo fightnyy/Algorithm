1d8c5e6ca558a97769ddeffd52c70faae249b336
fightnyy <fightnyy@naver.com>
2022-05-04 22:36:07 +0900

    add 905 leetcode

--- leetcode/905.py
+++ leetcode/905.py
@@ -1,10 +1,7 @@
+from collections import Counter
 class Solution:
-    def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        odds = []
-        evens = []
-        for num in nums:
-            if num % 2 == 0:
-                evens.append(num)
-            else:
-                odds.append(num)
-        return evens + odds
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        counter, ans = Counter([n for n in nums if n <= k]), 0
+        for key in counter:
+            ans += min(counter[key], counter[k - key])
+        return ans // 2
